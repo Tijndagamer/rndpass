6f69c2c319368aafa8758ac319bbd7725ff61b86
Martijn <mrtijn@riseup.net>
2017-07-31 15:11:31 +0200

    Add help option

--- include/rndpass.h
+++ include/rndpass.h
@@ -30,7 +30,8 @@
 #include <strings.h>
 
 const char *version_info = "rndpass 0.1\nCopyright (c) 2017 Martijn (MrTijn/Tijndagamer\nLicense GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n";
-enum { NORMAL, ALTERNATIVE, VERSION, EXTENDED } mode;
+const char *help_text = "Usage: rndpass [OPTION]\nGenerate random passwords.\n\n-l length\tspecify length of the password\n-v\tbe verbose\n-a\tuse alternative password generator\n-V\tshow version information\n-e\tallow all standard ASCII printable characters to be used (ignored in combination with -a)\n-h\tshow this help\n";
+enum { NORMAL, ALTERNATIVE, VERSION, EXTENDED, HELP } mode;
 
 // Function for operating modes
 void normal(bool verbose, int len, bool ext);
--- src/rndpass.c
+++ src/rndpass.c
@@ -1,21 +1,21 @@
-/*                                                                             
- * rndpass.c                                                                   
- * This file is part of rndpass.                                               
- *                                                                             
- * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)                             
- *                                                                             
- * rndpass is free software: you can redistribute it and/or modify             
- * it under the terms of the GNU General Public License as published by        
- * the Free Software Foundation, either version 3 of the License, or           
- * (at your option) any later version.                                         
- *                                                                             
- * rndpass is distributed in the hope that it will be useful,                  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of              
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               
- * GNU General Public License for more details.                                
- *                                                                             
- * You should have received a copy of the GNU General Public License           
- * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*           
+/*
+ * rndpass.c
+ * This file is part of rndpass.
+ *
+ * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)
+ *
+ * rndpass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rndpass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*
  */
 
 #include "rndpass.h"
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     bool verbose = false;
 
     mode = NORMAL;
-    while ((opt = getopt(argc, argv, "l:vaVe")) != -1) {
+    while ((opt = getopt(argc, argv, "l:vaVeh")) != -1) {
         switch (opt) {
             case 'l':
                 len = atoi(optarg) + 1;
@@ -44,6 +44,9 @@ int main(int argc, char *argv[])
                 break;
             case 'e':
                 mode = EXTENDED;
+                break;
+            case 'h':
+                mode = HELP;
                 break;
             default:
                 error(-1, 0, "Usage: %s [-l len] [-aevV]", argv[0]);
@@ -62,6 +65,9 @@ int main(int argc, char *argv[])
             break;
         case VERSION:
             printf("%s", version_info);
+            break;
+        case HELP:
+            printf("%s", help_text);
             break;
         default:
             error(-1, 0, "Unspecified mode");
