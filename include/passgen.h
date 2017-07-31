b71517aca081670ad72996452c478505e851dd95
Martijn <mrtijn@riseup.net>
2017-07-31 15:12:51 +0200

    Remove trailing whitespace in copyright header

--- include/passgen.h
+++ include/passgen.h
@@ -1,21 +1,21 @@
-/*                                                                             
- * passgen.h                                                                   
- * This file is part of rndpass.                                               
- *                                                                             
- * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)                             
- *                                                                             
- * rndpass is free software: you can redistribute it and/or modify             
- * it under the terms of the GNU General Public License as published by        
- * the Free Software Foundation, either version 3 of the License, or           
- * (at your option) any later version.                                         
- *                                                                             
- * rndpass is distributed in the hope that it will be useful,                  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of              
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               
- * GNU General Public License for more details.                                
- *                                                                             
- * You should have received a copy of the GNU General Public License           
- * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*           
+/*
+ * passgen.h
+ * This file is part of rndpass.
+ *
+ * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)
+ *
+ * rndpass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rndpass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*
  */
 
 #ifndef PASSGEN_h
--- src/passgen.c
+++ src/passgen.c
@@ -1,21 +1,21 @@
-/*                                                                             
- * passgen.c                                                                   
- * This file is part of rndpass.                                               
- *                                                                             
- * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)                             
- *                                                                             
- * rndpass is free software: you can redistribute it and/or modify             
- * it under the terms of the GNU General Public License as published by        
- * the Free Software Foundation, either version 3 of the License, or           
- * (at your option) any later version.                                         
- *                                                                             
- * rndpass is distributed in the hope that it will be useful,                  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of              
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               
- * GNU General Public License for more details.                                
- *                                                                             
- * You should have received a copy of the GNU General Public License           
- * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*           
+/*
+ * passgen.c
+ * This file is part of rndpass.
+ *
+ * Copyright (c) 2017 Martijn (MrTijn/Tijndagamer)
+ *
+ * rndpass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rndpass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rndpass.  If not, see <http://www.gnu.org/licenses/>.*
  */
 
 #include "passgen.h"
@@ -40,7 +40,7 @@ int countnull(unsigned char buffer[], size_t buflen)
 // Wrapper for getrandom syscall
 int getrandom(void *buffer, size_t buflen, unsigned int flags)
 {
-    return syscall(SYS_getrandom, buffer, buflen, flags);    
+    return syscall(SYS_getrandom, buffer, buflen, flags);
 }
 
 /* Generate random string, store in s, slen long. Returns total read random
